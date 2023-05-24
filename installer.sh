edee08a175f53b9eb593392d9b6035d9e2fd8c62
Ryan <110108058+dtnetwork-rpereira@users.noreply.github.com>
2023-05-24 16:13:35 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,13 +1,15 @@
 sudo pacman-mirrors -c Brazil
 sudo pacman -Sy
 
-sudo pacman -S --needed git base-devel
+sudo pacman -S --needed git base-devel flatpak
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
 sudo pacman -S docker docker-compose
-yay google-chrome visual-studio-code-bin teams
+yay google-chrome
+yay teams
+yay visual-studio-code-bin
 
 sudo systemctl enable docker
 sudo usermod -aG docker $1
