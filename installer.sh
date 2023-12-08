5628034e52e30a1feeb0414eafab9e046e7f7016
Ryan <110108058+dtnetwork-rpereira@users.noreply.github.com>
2023-12-08 10:28:06 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,7 +6,7 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-sudo pacman -S docker docker-compose python-pip python-pipx python-setuptools gst-plugins-good gst-plugin-pipewire php nodejs npm noto-fonts-emoji
+sudo pacman -S docker docker-compose python-pip python-pipx python-setuptools gst-plugins-good gst-plugin-pipewire php nodejs npm noto-fonts-emoji networkmanager-openvpn network-manager-applet
 fc-cache -f
 yay google-chrome
 yay teams-for-linux
